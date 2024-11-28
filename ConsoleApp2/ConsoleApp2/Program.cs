e831bc663daa5489171ecb500afa82f661905958
Borys <kamilborsuk38@gmail.com>
2024-11-28 09:19:30 +0100

    praca domowa dzien 4

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.ComponentModel.Design;
+
+var name= "Ewa";
+var sex = "female or male";
+var age = "30";
+
+if ( sex == "female && < 30");
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (name == "Ewa" && age == "30");
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+if (sex == "male" && age != "18");
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
